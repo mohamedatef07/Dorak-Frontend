4cbf75a75f727885ec1a21c2b0fb1b1f96b11083
Mohamed Atef <muhammad.atef44@gmail.com>
2025-05-11 22:31:08 +0300

    finish from login and interceptors

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-not-found',
   imports: [RouterLink],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
 export class NotFoundComponent {
-
+  authServices = inject(AuthService);
+  router = inject(Router);
+  HandelLogOut() {
+    this.authServices.logOut().subscribe({
+      next: (res) => {
+        this.router.navigate(['/login']);
+      },
+    });
+  }
 }
