56f85099c8025edfd00b599e2ad2682272897fdf
Mohamed Atef <muhammad.atef44@gmail.com>
2025-06-12 18:15:44 +0300

    redesign popup

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -1,53 +1,18 @@
 import { Injectable } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
-import { Subject } from 'rxjs';
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignalRService {
-  private hubConnection: signalR.HubConnection | null = null;
-  public queueStatusUpdate = new Subject<{ liveQueueId: number; newStatus: string }>();
-
-  constructor() {
-    this.startConnection();
-    this.registerOnServerEvents();
-  }
-
-  private startConnection() {
-    this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl('http://localhost:5139/queueHub')
-      .withAutomaticReconnect()
-      .build();
-
+  private hubConnection!: signalR.HubConnection;
+  constructor() {}
+  public startConnection() {
+    this.hubConnection = new signalR.HubConnectionBuilder().withUrl('').build();
     this.hubConnection
       .start()
-      .then(() => console.log('SignalR connection started'))
-      .catch(err => console.error('Error while starting SignalR connection: ', err));
-
-
-    this.hubConnection.onreconnected(() => console.log('SignalR reconnected'));
-    this.hubConnection.onclose(() => console.log('SignalR connection closed, attempting to reconnect...'));
-  }
-
-  private registerOnServerEvents() {
-    if (this.hubConnection) {
-      this.hubConnection.on('ReceiveQueueStatusUpdate', (liveQueueId: number, newStatus: string) => {
-        this.queueStatusUpdate.next({ liveQueueId, newStatus });
-      });
-    } else {
-      console.warn('SignalR connection not established, cannot register events.');
-    }
-  }
-
-  public reconnectIfNeeded() {
-    if (!this.hubConnection || this.hubConnection.state === signalR.HubConnectionState.Disconnected) {
-      this.startConnection();
-      this.registerOnServerEvents();
-    }
-  }
-
-  public getConnectionState(): signalR.HubConnectionState {
-    return this.hubConnection ? this.hubConnection.state : signalR.HubConnectionState.Disconnected;
+      .then(() => console.log('SignalR Connection started'))
+      .catch((err) =>
+        console.log(`Error establishing SignalR connection ${err}`)
+      );
   }
 }
