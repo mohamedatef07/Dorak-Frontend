679b74765e02cd9cb8f21254a85e78fbd4203b2b
Amgad Montasser Adly <amgadnaser7@gmail.com>
2025-06-16 14:44:44 +0300

    Checkout2

--- src/app/features/client/components/checkout/checkout.component.ts
+++ src/app/features/client/components/checkout/checkout.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
 import { ICheckoutRequest } from '../../models/ICheckoutRequest';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-checkout',
+  imports: [CommonModule,FormsModule],
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
@@ -14,12 +17,12 @@ export class CheckoutComponent implements OnInit {
   checkoutRequest: ICheckoutRequest = {
     AppointmentId: 0,
     ClientId: '',
-    StripeToken: '',
+    StripeToken: 'tok_visa',
     Amount: 0
   };
 
   constructor(private _clientservice:ClientService,private route: ActivatedRoute) { }
-  checkinformation:ICheckoutRequest= {} as ICheckoutRequest
+  
 
   paymentMethods = [
     { label: 'Credit Card', value: 'credit' },
@@ -27,8 +30,11 @@ export class CheckoutComponent implements OnInit {
     { label: 'Easy Pay', value: 'easypay' }
   ];
 
+  selectedPaymentMethod: string = 'credit';  // Default payment method
+
   clientcheckout():void{
-    this._clientservice.Checkout(this.checkinformation).subscribe({
+    console.log(this.checkoutRequest);
+    this._clientservice.Checkout(this.checkoutRequest).subscribe({
       next:(res)=>{
         console.log(res)
       },
@@ -38,10 +44,26 @@ export class CheckoutComponent implements OnInit {
     })
   }
 
+
+
+  onPaymentMethodChange(paymentMethod: string): void {
+    this.selectedPaymentMethod = paymentMethod;
+
+    // Update Stripe token if 'Credit Card' is selected
+    if (this.selectedPaymentMethod === 'credit') {
+      this.checkoutRequest.StripeToken = 'tok_visa';  // Set the Stripe token (replace with real token)
+    } else {
+      this.checkoutRequest.StripeToken = '';  // Clear Stripe token if another payment method is selected
+    }
+  }
+
+
+
   ngOnInit() {
     const state = history.state;
     if (state && state.checkoutRequest) {
       this.checkoutRequest = state.checkoutRequest;
+      this.checkoutRequest.StripeToken = 'tok_visa'; // Default Stripe token for testing
     }
     console.log('Checkout Request:', this.checkoutRequest);
   }
