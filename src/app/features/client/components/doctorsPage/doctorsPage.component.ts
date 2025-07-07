ae4cf25d9f2c76df1ab8566307178d1ddf9cba4b
Mohamed Atef <muhammad.atef44@gmail.com>
2025-07-07 23:13:03 +0300

    create provider card ui

--- src/app/features/client/components/doctorsPage/doctorsPage.component.ts
+++ src/app/features/client/components/doctorsPage/doctorsPage.component.ts
@@ -13,13 +13,24 @@ import { GenderType } from '../../../../Enums/GenderType.enum';
 import { SelectModule } from 'primeng/select';
 import { CheckboxModule } from 'primeng/checkbox';
 import { buildDoctorFilter } from '../../../../utils/DoctorFilterBuilder';
+import { AvatarModule } from 'primeng/avatar';
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-doctorsPage',
   standalone: true,
   templateUrl: './doctorsPage.component.html',
   styleUrls: ['./doctorsPage.component.css'],
-  imports: [CommonModule, FormsModule, ClientFooterComponent, NavBarComponent,SelectModule, CheckboxModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ClientFooterComponent,
+    NavBarComponent,
+    SelectModule,
+    CheckboxModule,
+    AvatarModule,
+    RatingModule,
+  ],
 })
 export class DoctorsPageComponent implements OnInit {
   doctors: IDoctorCard[] = [];
@@ -28,8 +39,8 @@ export class DoctorsPageComponent implements OnInit {
   searchText: string = '';
   specialty: string = '';
   city: string = '';
+  fullImagePath: string = `${environment.apiUrl}`;
 
-  fullImagePath: string = '';
   selectedTitles: number[] = [];
   selectedGenders: number[] = [];
   selectedCities: string[] = [];
@@ -42,12 +53,16 @@ export class DoctorsPageComponent implements OnInit {
   ];
   cities: string[] = ['naser city', 'Giza', 'Alexandria', 'Aswan'];
 
-  titles: Array<{ value: number, label: string }> = Object.entries(DoctorTitle)
-    .filter(([key, value]) => typeof value === 'number' && value !== DoctorTitle.None)
+  titles: Array<{ value: number; label: string }> = Object.entries(DoctorTitle)
+    .filter(
+      ([key, value]) => typeof value === 'number' && value !== DoctorTitle.None
+    )
     .map(([key, value]) => ({ value: value as number, label: key }));
 
-  genders: Array<{ value: number, label: string }> = Object.entries(GenderType)
-    .filter(([key, value]) => typeof value === 'number' && value !== GenderType.None)
+  genders: Array<{ value: number; label: string }> = Object.entries(GenderType)
+    .filter(
+      ([key, value]) => typeof value === 'number' && value !== GenderType.None
+    )
     .map(([key, value]) => ({ value: value as number, label: key }));
 
   filterModel: any = {
@@ -75,14 +90,11 @@ export class DoctorsPageComponent implements OnInit {
     this.getAllDoctors();
   }
 
-  getAllDoctors(filter: Partial<IDoctorFilter>={}): void {
+  getAllDoctors(filter: Partial<IDoctorFilter> = {}): void {
     debugger;
     this.cardDoctorService.getAllDoctorsCards(filter).subscribe({
       next: (res) => {
         this.doctors = [...res.Data];
-        if (this.doctors[0]?.Image) {
-          this.fullImagePath = `${environment.apiUrl}${this.doctors[0].Image}`;
-        }
         this.filteredDoctors = [...this.doctors];
       },
       error: (err) => {
@@ -94,11 +106,12 @@ export class DoctorsPageComponent implements OnInit {
   onTitleChange(event: any): void {
     const value = Number(event.target.value);
     if (event.target.checked) {
-      if (!this.selectedTitles.includes(value)) this.selectedTitles.push(value);
+      if (!this.selectedTitles.includes(value)) {
+        this.selectedTitles.push(value);
+      }
     } else {
-      this.selectedTitles = this.selectedTitles.filter(t => t !== value);
+      this.selectedTitles = this.selectedTitles.filter((t) => t !== value);
     }
-    console.log('Selected titles:', this.selectedTitles);
     this.applyFilters();
   }
 
@@ -107,7 +120,7 @@ export class DoctorsPageComponent implements OnInit {
     if (event.target.checked) {
       if (!this.selectedCities.includes(value)) this.selectedCities.push(value);
     } else {
-      this.selectedCities = this.selectedCities.filter(c => c !== value);
+      this.selectedCities = this.selectedCities.filter((c) => c !== value);
     }
     this.applyFilters();
   }
@@ -115,16 +128,19 @@ export class DoctorsPageComponent implements OnInit {
   onGenderChange(event: any): void {
     const value = Number(event.target.value);
     if (event.target.checked) {
-      if (!this.selectedGenders.includes(value)) this.selectedGenders.push(value);
+      if (!this.selectedGenders.includes(value))
+        this.selectedGenders.push(value);
     } else {
-      this.selectedGenders = this.selectedGenders.filter(g => g !== value);
+      this.selectedGenders = this.selectedGenders.filter((g) => g !== value);
     }
-    console.log('Selected genders:', this.selectedGenders);
     this.applyFilters();
   }
 
   onSpecializationChange(specialization: string): void {
-    if (specialization && !this.selectedSpecializations.includes(specialization)) {
+    if (
+      specialization &&
+      !this.selectedSpecializations.includes(specialization)
+    ) {
       this.selectedSpecializations = [specialization];
     } else {
       this.selectedSpecializations = [];
@@ -147,20 +163,10 @@ export class DoctorsPageComponent implements OnInit {
         ? new Date(this.filterModel.AvailableDate).toISOString().split('T')[0]
         : undefined,
     };
-    console.log('Sending filter:', filter);
     this.getAllDoctors(filter);
   }
 
-  // searchDoctors(): void {
-  //   this.filterModel.SearchText = this.searchText;
-  //   this.filterModel.City = this.city || undefined;
-  //   this.filterModel.Specialization = this.specialty || undefined;
-
-  //   this.applyFilters();
-
-  //   // Reset search inputs after applying
-  //   this.searchText = '';
-  //   this.city = '';
-  //   this.specialty = '';
-  // }
+  trackByIndex(index: number, item: any): number {
+    return index;
+  }
 }
