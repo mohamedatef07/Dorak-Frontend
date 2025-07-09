0289670ffa6d1bb9ed44f3bd093a4bf0c3097c5f
Mahmoud Abdou <157692655+Mabdou743@users.noreply.github.com>
2025-07-10 00:29:01 +0300

    Contact Us, Help - Support, Terms and Conditions

--- src/app/features/owner/components/owner-footer/owner-footer.component.ts
+++ src/app/features/owner/components/owner-footer/owner-footer.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-owner-footer',
   templateUrl: './owner-footer.component.html',
-  styleUrls: ['./owner-footer.component.css']
+  styleUrls: ['./owner-footer.component.css'],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive]
 })
 export class OwnerFooterComponent implements OnInit {
 
