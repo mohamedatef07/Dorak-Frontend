945635047103313f385c37e92823bd5734550b74
Amgad Montasser Adly <amgadnaser7@gmail.com>
2025-07-10 16:12:57 +0300

    DropDown Provider

--- src/app/features/owner/components/assign-service-to-provider-center/assign-service-to-provider-center.component.ts
+++ src/app/features/owner/components/assign-service-to-provider-center/assign-service-to-provider-center.component.ts
@@ -1,9 +1,13 @@
+import { AuthService } from './../../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { S_ServicesService } from '../../services/S_Services.service';
 import { IShiftServices } from '../../models/IShiftServices';
 import { IAddProviderCenterService } from '../../models/IAddProviderCenterService';
 import { CommonModule } from '@angular/common';
+import { CenterService } from '../../services/Center.service';
+import { DropdownModule } from 'primeng/dropdown';
+import { IProviderDropDown } from '../../models/IProviderDropDown';
 
 @Component({
   selector: 'app-assign-service-to-provider-center',
@@ -11,30 +15,36 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./assign-service-to-provider-center.component.css'],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DropdownModule
   ]
 })
 export class AssignServiceToProviderCenterComponent implements OnInit {
   assignForm!: FormGroup;
   services: IShiftServices[] = [];
+  providers: IProviderDropDown[] = [];
   loading = false;
   successMessage = '';
   errorMessage = '';
-
+  centerId!: number;
   constructor(
     private fb: FormBuilder,
-    private sServices: S_ServicesService
+    private sServices: S_ServicesService,
+    private AuthService: AuthService,
+    private centerService: CenterService
   ) {}
 
   ngOnInit(): void {
+    this.centerId = this.AuthService.getCenterId();
     this.assignForm = this.fb.group({
-      ProviderId: ['', Validators.required],
+      ProviderId: [null, Validators.required],
       ServiceId: [null, Validators.required],
-      CenterId: [null, Validators.required],
+      CenterId: [this.centerId, Validators.required],
       Price: [null, [Validators.required, Validators.min(0)]],
       Priority: [null, [Validators.required, Validators.min(1)]]
     });
     this.fetchServices();
+    this.fetchProviders();
   }
 
   fetchServices() {
@@ -44,6 +54,17 @@ export class AssignServiceToProviderCenterComponent implements OnInit {
       },
       error: () => {
         this.errorMessage = 'Failed to load services.';
+      }
+    });
+  }
+
+  fetchProviders() {
+    this.centerService.getProvidersDropDown(this.centerId).subscribe({
+      next: (res) => {
+        this.providers = res.Data;
+      },
+      error: () => {
+        this.errorMessage = 'Failed to load providers.';
       }
     });
   }
