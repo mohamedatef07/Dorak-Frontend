3fe27f21787f2919fc63586fd63238e6b480fb53
Fatma Naser <fatmanaser685@gmail.com>
2025-07-14 11:53:39 -0700

    editlandingpage

---
+++ src/app/features/landingpage/components/Industries/Industries.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { faClock, faZap, faBuilding, faCheckCircle,faCity, faBolt,faGlobe,faDesktop, faArrowRight, faChevronDown, faChevronUp, faHospital, faLandmark, faGraduationCap, faPhone, faStethoscope, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import {
+  AfterViewInit,
+  ElementRef,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
+
+
+@Component({
+  selector: 'app-Industries',
+  templateUrl: './Industries.component.html',
+  styleUrls: ['./Industries.component.css']
+})
+export class IndustriesComponent  {
+
+ 
+}
---
+++ src/app/features/landingpage/components/landingPage-footer/landingPage-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landingPage-footer',
+  templateUrl: './landingPage-footer.component.html',
+  styleUrls: ['./landingPage-footer.component.css']
+})
+export class LandingPageFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
